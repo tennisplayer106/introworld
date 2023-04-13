1f7c2e4fbb6b194f96e92b3dd0a9ee3cf19d5ea5
tennisplayer106 <76907264+tennisplayer106@users.noreply.github.com>
2023-04-14 00:04:34 +0530

    Add files via upload

---
+++ utils.js
@@ -0,0 +1,21 @@
+function click1(){
+    window.open( "index.html","_blank");
+  }
+  
+  function click2(){
+    window.open( "login.html","_blank");
+  }
+  
+  function click3(){
+    window.open( "signUp.html","_blank");
+  }
+  
+  function click4(){
+    window.open( "forum.html","_blank");
+  }
+  
+  
+  
+  
+  
+  
